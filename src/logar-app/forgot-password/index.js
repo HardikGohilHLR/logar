5034929f24e3c6a58fe68b11325e03a57ac600cd
Hardik Gohil <32103022+HardikGohilHLR@users.noreply.github.com>
2022-11-25 23:05:16 +0530

    style changes

--- src/logar-app/forgot-password/index.js
+++ src/logar-app/forgot-password/index.js
@@ -30,7 +30,7 @@ const ForgotPassword = () => {
                             </div>
 
                             <p className="text-center form-back-login">
-                                <Link to="/login">
+                                <Link to="/">
                                     <span>
                                         <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                                             <path d="M11.4375 18.75L4.6875 12L11.4375 5.25M5.625 12H19.3125" stroke="#1D2B3B" stroke-width="2.25" stroke-linecap="round" stroke-linejoin="round"/>
